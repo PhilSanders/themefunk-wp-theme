6d0d4503a9093fb0d62bff81255590123486273d
Phil Sanders <philsanders79@gmail.com>
2014-12-16 01:24:42 -0500

    Cleaned whitespace from header and footer

--- header.php
+++ header.php
@@ -16,97 +16,82 @@
 
 <body <?php body_class(); ?>>
 
-<div class="off-canvas-wrap" data-offcanvas> 
+    <div class="off-canvas-wrap" data-offcanvas>
 
-<div class="inner-wrap">
+        <div class="inner-wrap">
 
-<nav class="tab-bar show-for-small"> 
-  <section class="right-small"> <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a> </section> 
-</nav> 
+            <nav class="tab-bar show-for-small">
+              <section class="right-small"> <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a> </section>
+            </nav>
 
-<aside class="right-off-canvas-menu show-for-small"> 
-  <label>Navigation</label>
-<?php 
-	 
-    $is_nav = '' ;
-    $defaults = array(
-		'theme_location'  => 'top',
-		'menu'            => '',
-		'container'       => false,
-		'container_class' => '',
-		'container_id'    => '',
-		'menu_class'      => '',
-		'menu_id'         => 'off-canvas-nav',
-		'echo'            => false,
-		'fallback_cb'     => '',
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul class="off-canvas-list">%3$s</ul>',
-		'depth'           => 0,
-		'walker'          => ''
-	);
+            <aside class="right-off-canvas-menu show-for-small">
+              <label>Navigation</label>
+            <?php
 
-	$is_nav = wp_nav_menu( $defaults );
-	if ($is_nav == '') { 
-		netfunktheme_default_navigation();
-	 } else {
-		echo ($is_nav); 
-	}
-?>
+                $is_nav = '' ;
+                $defaults = array(
+                    'theme_location'  => 'top',
+                    'menu'            => '',
+                    'container'       => false,
+                    'container_class' => '',
+                    'container_id'    => '',
+                    'menu_class'      => '',
+                    'menu_id'         => 'off-canvas-nav',
+                    'echo'            => false,
+                    'fallback_cb'     => '',
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul class="off-canvas-list">%3$s</ul>',
+                    'depth'           => 0,
+                    'walker'          => ''
+                );
 
-</aside>
+                $is_nav = wp_nav_menu( $defaults );
+                if ($is_nav == '') {
+                    netfunktheme_default_navigation();
+                 } else {
+                    echo ($is_nav);
+                }
+            ?>
 
-<section class="main-section">
+            </aside>
 
-<div id="wrapper" class="hfeed">
+            <section class="main-section">
+                <div id="wrapper" class="hfeed">
 
-<header>
+                <header class="contain-to-grid fixed">
+                  <div class="row">
+                    <div class="small-12 columns clearfix">
+                    <nav class="top-bar" data-topbar role="navigation">
+                      <ul class="title-area">
+                        <li class="logo-icon">
+                          <?php
+                            $header_image = get_header_image();
+                            if ( !empty( $header_image ) ) { ?>
+                                <div id="logo" class="left"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                                <img src="<?php header_image(); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+                                </a></div>
+                            <?php
+                            } else { ?>
+                                <div id="logo2"><a href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a></div><!--logo end-->
+                            <?php } ?>
+                        </li>
+                        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+                        <!--li class="toggle-topbar menu-icon">
+                          <a href="#"><span></span></a>
+                        </li-->
+                      </ul>
 
-  <div class="row">
-  
-    <div class="small-12 columns clearfix">
+                      <section class="top-bar-section hide-for-small">
+                        <!-- right nav section -->
+                        <?php do_action('netfunktheme_navigation_menu'); ?>
+                        <!-- left vav section -->
+                        <?php do_action ('netfunktheme_user_menu') ?>
+                      </section>
+                    </nav>
+                    </div>
 
-    <nav class="top-bar" data-topbar data-options="">
-
-      <ul class="title-area">
-
-        <li class="logo-icon">
-          <?php 
-			$header_image = get_header_image();
-            if ( !empty( $header_image ) ) { ?>
-                <div id="logo" class="left"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                <img src="<?php header_image(); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-                </a></div>
-            <?php 
-			} else { ?>
-                <div id="logo2"><a href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a></div><!--logo end-->
-            <?php } ?>
-        </li>
-
-        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-
-        <!--li class="toggle-topbar menu-icon">
-          <a href="#"><span></span></a>
-        </li-->
-
-      </ul>
-
-      <section class="top-bar-section hide-for-small">
-
-        <!-- right nav section -->
-		<?php do_action('netfunktheme_navigation_menu'); ?>
-
-        <!-- left vav section -->
-        <?php do_action ('netfunktheme_user_menu') ?>
-
-      </section>
-
-    </nav>
-
-    </div>
-
-  </div><!-- top-bar -->
-
-</header>
+                  </div><!-- top-bar -->
+                </header>
